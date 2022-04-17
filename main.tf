474c34132e0ec4d204377dc68888e6ba8e4d6444
Tai Nguyen <dtnguyen22@myseneca.ca>
2022-04-17 19:38:36 -0400

    add rg

--- main.tf
+++ main.tf
@@ -17,13 +17,12 @@ provider "azurerm" {
     client_secret   =   "XCGY_uH0vsQ1koAE1lzH5j~KSxmmCRGR4J"
 }
 
-/*
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
 }
 
-
+/*
 resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
   address_space       = ["10.0.0.0/16"]
