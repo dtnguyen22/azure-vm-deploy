c0ed9ad956017ff419cb2ef1f24ba2b48b68cf24
Tai Nguyen <dtnguyen22@myseneca.ca>
2022-04-19 04:40:07 -0400

    fix

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "vm_size" {
   type        = string
   default     = "Standard_B2s"
 }
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources"
+  type        = string
+  default     = "k8s"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources should be created."
+  type        = string
+  default     = "Canada Central"
+}
